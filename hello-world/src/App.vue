31afd8c8a589f72166814b2bf113f5bdc72ee8bb
Jayanta Biswas <bjayanta.me@gmail.com>
2019-10-03 18:13:49 +0600

    Basic lesson and a project setup.

---
+++ hello-world/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+    <h1>Hello World!</h1>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
